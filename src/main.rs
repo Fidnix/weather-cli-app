b5dbc6c1c7cbe9ce728409142ce9d4dfa0ce0255
Fidnix <fidel.moises0@gmail.com>
2025-03-09 17:41:03 -0500

    feat: Turning to a CLI

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,39 @@
-use std::io; // Importing the standard input/output library for user input
-
 use colored::*; // Importing colored crate for text coloring
 use dotenvy::dotenv;
 use std::env;
 
+use clap::Parser;
+
 use weather::WeatherResponse;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short='c', long, help="City name. e.g Paris")]
+    city: String,
+
+    #[arg(short='d', long, help="Country code. e.g FR (France)")]
+    country_code: String,
+}
 
 fn main() {
     dotenv().ok();
+    let args = Args::parse();
+
     println!("{}", "Welcome to Weather Station!".bright_yellow()); // Displaying welcome message
 
-    loop {
-        println!("{}", "Please enter the name of the city:".bright_green()); // Prompting user to enter city name
+    // Get your API key from OpenWeatherMap
+    let api_key = env::var("OPENWEATHER_API_KEY").expect("OPENWEATHER_API_KEY no está definida");
 
-        let mut city = String::new();
-        io::stdin().read_line(&mut city).expect("Failed to read input"); // Reading user input for city name
-        let city = city.trim();
-
-        println!("{}", "Please enter the country code (e.g., US for United States):".bright_green()); // Prompting user to enter country code
-
-        let mut country_code = String::new();
-        io::stdin().read_line(&mut country_code).expect("Failed to read input"); // Reading user input for country code
-        let country_code = country_code.trim();
-
-        // Get your API key from OpenWeatherMap
-        let api_key = env::var("OPENWEATHER_API_KEY").expect("DATABASE_URL no está definida");
-
-        // Calling the function to fetch weather information
-        match WeatherResponse::get_info(&city, &country_code, &api_key) {
-            Ok(response) => {
-                WeatherResponse::display_info(&response); // Displaying weather information
-            }
-            Err(err) => {
-                eprintln!("Error: {}", err); // Printing error message in case of failure
-            }
+    // Calling the function to fetch weather information
+    match WeatherResponse::get_info(&args.city, &args.country_code, &api_key) {
+        Ok(response) => {
+            WeatherResponse::display_info(&response); // Displaying weather information
         }
-
-        println!("{}", "Do you want to search for weather in another city? (yes/no):".bright_green()); // Prompting user to continue or exit
-        let mut input = String::new();
-        io::stdin().read_line(&mut input).expect("Failed to read input"); // Reading user input for continuation
-        let input = input.trim().to_lowercase();
-
-        if input != "yes" {
-            println!("Thank you for using our software!");
-            break; // Exiting the loop if user doesn't want to continue
+        Err(err) => {
+            eprintln!("Error: {}", err); // Printing error message in case of failure
         }
     }
+
+    println!("{}", "Thank you for using our software!".bright_magenta());
 }
